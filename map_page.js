2a324c535deedf644a47bf2f99bc316ea2c525dd
Kenyab01 <147757600+Kenyab01@users.noreply.github.com>
2024-12-11 22:50:15 -0500

    hi

--- map_page.js
+++ map_page.js
@@ -8,6 +8,32 @@ function createMap() {
     return route_map;  // Return the map object
 }
 
+function bus_stops(){
+    const api_url = 'http://api.wmata.com/Bus.svc/json/jRoutes';
+    const api_key = ff1282b4e3ea4a70874cf83bf6510a26;
+
+    fetch(api_url, {
+        method: 'GET',
+        headers: {
+            'api_key': api_key
+        }
+    })
+    .then (response => response.json())
+    .then(data => {
+        console.log(data)
+    }
+    
+    if(data.Stops){
+        data.Stops.forEach(bus => {
+            const stop_location = [Stops.Lat, Stops.Lon]
+            bus_routes(stop_location);            
+        });
+    } else {
+        console.error('route not found');
+}
+})
+.catch(error)
+
 window.onload = () => {
     createMap();  // Call the createMap function when the window finishes loading
 };
